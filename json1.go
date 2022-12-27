0ee3b838335543822f49eadc3aec729df017f390
Louis Kwezi <kwezi.louishg@gmail.com>
2022-12-27 20:16:15 +0300

    justadd

--- json1.go
+++ json1.go
@@ -18,7 +18,7 @@ func main() {
 	// 	"kim", 20, "Uganda",
 	// }
 	person2 := Person{
-		Name:"kim",Age: 47,
+		Name:"kim",Age: 47,Location:"Uganda",
 	}
 
 	personArray, err := json.Marshal(person2)
