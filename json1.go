cb24995b3087a7f0cfdf70412c5d1464cab8e05e
Louis Kwezi <kwezi.louishg@gmail.com>
2022-12-27 19:24:21 +0300

    https://github.com/louismomo66/go_project

---
+++ json1.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type Person struct {
+	Name     string `json:"firstName"`
+	Age      float64
+	Location string `json:"location,omitempty"`
+}
+
+func main() {
+	
+	// person := Person{
+	// 	"kim", 20, "Uganda",
+	// }
+	person2 := Person{
+		Name:"kim2",Age: 28,
+	}
+
+	personArray, err := json.Marshal(person2)
+
+	if err != nil{
+		log.Fatalf("Unable to Encode")
+	}
+
+	fmt.Println(string(personArray))
+
+}
