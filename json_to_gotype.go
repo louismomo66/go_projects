55301497e48d863549439e79abdcf3dbb7e83078
Louis Kwezi <kwezi.louishg@gmail.com>
2022-12-27 20:46:43 +0300

    justadd

---
+++ json_to_gotype.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type Person1 struct{
+	Name string
+	Age float64
+	Location string
+}
+func main(){
+
+j :=[]byte(`{"name":"Ramony","age":43.4,"location":"Texas"}`)
+
+var p Person1
+
+err := json.Unmarshal(j,&p)
+
+if err != nil{
+	log.Fatalf("Unabele to Decode The Json")
+
+	
+}
+fmt.Printf("Name:%s\nAge:%d\nLocation:%s\n",p.Name,p.Age,p.Location)
+
+}
